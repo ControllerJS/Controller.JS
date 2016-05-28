f8228b5792bdb4693b10bf04fb9871a13af228cd
Tom Valk <tomvalk@gmail.com>
2016-05-28 17:32:51 +0200

    Adding gbxremote typing.

--- typings/custom/gbxremote/index.d.ts
+++ typings/custom/gbxremote/index.d.ts
@@ -1,4 +1,9 @@
 
 declare module 'gbxremote' {
+  import EventEmitter = NodeJS.EventEmitter;
+  export interface Gbx extends EventEmitter {
+    createClient (port: number, address: string) : Gbx;
 
+    query (name: string, params?: any[]): Promise<any>;
+  }
 }
