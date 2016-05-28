05d991cefc2bc3106e02dacb50bdfb4a4fa7a67e
Tom Valk <tomvalk@gmail.com>
2016-05-28 16:51:13 +0200

    Database ts.

--- src/Database/Client.ts
+++ src/Database/Client.ts
@@ -6,102 +6,98 @@ import {Sequelize} from 'sequelize';
 import {App} from '../app';
 import {Configuration, DatabaseDialect} from './../util/configuration';
 
-/// <reference path="index.ts" />
-export namespace Database {
+export class Client {
 
-  export class Client {
+  private app: App;
+  private sequelize: Sequelize;
 
-    private app: App;
-    private sequelize: Sequelize;
+  private options: any;
 
-    private options: any;
+  private database: string;
+  private username: string;
+  private password: string;
 
-    private database: string;
-    private username: string;
-    private password: string;
+  constructor () {
+    this.app = App.instance;
+    this.sequelize = null;
 
-    constructor () {
-      this.app = App.instance;
-      this.sequelize = null;
+    let config = Configuration.instance.config.config.db;
+    let options = {
+      pool: config.pool
+    };
 
-      let config = Configuration.instance.config.config.db;
-      let options = {
-        pool: config.pool
-      };
+    if (config.dialect ===  DatabaseDialect.mysql
+      || config.dialect === DatabaseDialect.mariadb
+      || config.dialect === DatabaseDialect.mssql
+      || config.dialect === DatabaseDialect.postgres) {
 
-      if (config.dialect ===  DatabaseDialect.mysql
-        || config.dialect === DatabaseDialect.mariadb
-        || config.dialect === DatabaseDialect.mssql
-        || config.dialect === DatabaseDialect.postgres) {
-
-        Object.assign(options, {
-          host: config[config.dialect].host,
-          port: config[config.dialect].port,
-          dialect: config.dialect
-        });
-      }
-
-      if (config.dialect === DatabaseDialect.sqlite) {
-        Object.assign(options, {
-          dialect: 'sqlite',
-          storage: (
-            config.sqlite.storage.startsWith('./') ?
-              path.normalize(location + '/' + config.sqlite.storage)
-              :
-              path.resolve(config.sqlite.storage)
-          )
-        });
-      }
-
-      if (this.app.config.config.db.debug) {
-        Object.assign(options, {
-          logging: (input) => {this.app.log.debug(input);}
-        });
-      } else {
-        Object.assign(options, {
-          logging: false
-        });
-      }
-
-      this.options = options;
-      this.database = config.database;
-      this.username = config.authentication.username;
-      this.password = config.authentication.password;
+      Object.assign(options, {
+        host: config[config.dialect].host,
+        port: config[config.dialect].port,
+        dialect: config.dialect
+      });
     }
 
-    /**
-     * Connect to the database.
-     * @async
-     */
-    public connect () {
-      this.sequelize = new Sequelize.Sequelize(this.database, this.username, this.password, this.options);
+    if (config.dialect === DatabaseDialect.sqlite) {
+      Object.assign(options, {
+        dialect: 'sqlite',
+        storage: (
+          config.sqlite.storage.startsWith('./') ?
+            path.normalize(location + '/' + config.sqlite.storage)
+            :
+            path.resolve(config.sqlite.storage)
+        )
+      });
     }
 
-    /**
-     * Sync Database
-     * @return {Promise<any>}
-     * @async
-     */
-    public async sync () {
-      return this.sequelize.sync();
+    if (this.app.config.config.db.debug) {
+      Object.assign(options, {
+        logging: (input) => {this.app.log.debug(input);}
+      });
+    } else {
+      Object.assign(options, {
+        logging: false
+      });
     }
 
-    /**
-     * Load All Models for the Core.
-     */
-    public async loadCoreModels () {
-      let list = glob.sync(path.normalize(__dirname + '/../models/') + '*.js');
-
-      if (list.length > 0) {
-        list.forEach((file) => {
-          // Import sequelize model.
-          let model:any = this.sequelize.import(file);
-
-          // Save to model storage of app.
-          this.app.models[model.name] = model;
-        });
-      }
-    }
-
+    this.options = options;
+    this.database = config.database;
+    this.username = config.authentication.username;
+    this.password = config.authentication.password;
   }
+
+  /**
+   * Connect to the database.
+   * @async
+   */
+  public connect () {
+    this.sequelize = new Sequelize.Sequelize(this.database, this.username, this.password, this.options);
+  }
+
+  /**
+   * Sync Database
+   * @return {Promise<any>}
+   * @async
+   */
+  public async sync () {
+    return this.sequelize.sync();
+  }
+
+  /**
+   * Load All Models for the Core.
+   */
+  public async loadCoreModels () {
+    let list = glob.sync(path.normalize(__dirname + '/../models/') + '*.js');
+
+    if (list.length > 0) {
+      list.forEach((file) => {
+        // Import sequelize model.
+        let model:any = this.sequelize.import(file);
+
+        // Save to model storage of app.
+        this.app.models[model.name] = model;
+      });
+    }
+  }
+
 }
---
+++ src/Database/index.ts
@@ -0,0 +1,60 @@
+
+import {BaseFacade} from '../util/facade';
+import {Client} from './Client';
+
+export module Database {
+  /**
+   * Database Facade
+   */
+  export class Facade extends BaseFacade {
+    /**
+     * Database Client.
+     */
+    private client: Client;
+
+    constructor () {
+      super();
+    }
+
+    /**
+     * Connect to database.
+     */
+    public async init () {
+      this.app.log.debug('Connecting to database...');
+      await this.client.connect();
+    }
+
+    /**
+     * Register and sync with database.
+     */
+    public async run () {
+      this.app.log.debug('Registering models...');
+
+      await this.defineModels();
+      await this.client.sync();
+    }
+
+    /**
+     * Stop.
+     */
+    public async stop () {
+
+    }
+
+    /**
+     * Define and register models (sequelize).
+     */
+    private async defineModels () {
+      try {
+        // Load core models
+        await this.client.loadCoreModels();
+
+        // Plugin models.
+        // await this.app.pluginFacade.manager.loadModels(this.client.sequelize);
+      } catch (error) {
+        this.app.log.fatal('Fatal error with syncing/connecting to the database', error.stack);
+        process.exit(1);
+      }
+    }
+  }
+}
